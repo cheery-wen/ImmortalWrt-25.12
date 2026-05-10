73c1bf3bbc85df63113535d541795d70bf8a3b92
cheery-wen <90075857+cheery-wen@users.noreply.github.com>
2026-05-10 16:05:47 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -48,6 +48,29 @@ rm -rf dl/go-mod-cache 2>/dev/null || true
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+# 修改主机名
+sed -i "s/hostname='ImmortalWrt'/hostname='OpenWrt'/g" package/base-files/files/bin/config_generate
+
+# 清除默认密码
+sed -i 's/root::0:0:99999:7:::/root::::::::/g' package/base-files/files/etc/shadow 2>/dev/null || true
+
+# 获取 Git 的修订版本号 (如 R26.02.20)
+GET_REVISION="R$(date +%y.%m.%d)"
+
+# 修改固件版本
+cat > package/base-files/files/etc/openwrt_release <<EOF
+DISTRIB_ID='ImmortalWrt'
+DISTRIB_RELEASE='25.12'
+DISTRIB_REVISION='$GET_REVISION'
+DISTRIB_TARGET='x86/64'
+DISTRIB_ARCH='x86_64'
+DISTRIB_DESCRIPTION='OpenWrt ($(date +%Y.%m.%d)) compiled by cheery)$GET_REVISION / LuCI openwrt-25.12 branch'
+DISTRIB_TAINTS='no-all busybox'
+EOF
+
 # 默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile || true
 
